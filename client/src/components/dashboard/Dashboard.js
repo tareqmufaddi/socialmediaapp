e897f5f77c7a86f828f48d7e9619d50a297d5d68
Tareq Mufaddi <tareq.mufaddi@gmail.com>
2019-11-01 19:22:13 +0200

    privateDashboardWorking

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+//racfp
+const Dashboard = props => {
+  return <div>Dashboard</div>;
+};
+
+Dashboard.propTypes = {};
+
+export default Dashboard;
